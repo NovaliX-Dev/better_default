4336b879b766e255d45e9cd74c29406486d01af8
NovaliX <novalix@protonmail.com>
2024-06-25 17:42:18 +0200

    Upgraded publish script

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,14 +1,24 @@
 import os
 
+CHECK_QUEUE = [
+    ("Modified files present", "git diff-index --quiet HEAD")
+]
+
 COMMAND_QUEUE = [
     "cargo test",
     "rustdoc-include --root ./",
+    "git commit -a --message \"Pre-publish commit\""
     "cargo publish --dry-run"
 ]
 
 def execute(command: str) -> int:
     if os.name == "nt":
         return os.system(command)
+
+for (name, command) in CHECK_QUEUE:
+    if execute(command) != 0:
+        print(f"`{name}` check failed.")
+        exit(2)
 
 for command in COMMAND_QUEUE:
     if execute(command) != 0:
